1f10833ec1ae9c6248d8817c2804c68369270d20
Michael Mason <mjmasn@users.noreply.github.com>
2019-11-28 14:43:34 +0000

    Add some more cases

--- App.js
+++ App.js
@@ -15,15 +15,28 @@ const App: () => React$Node = () => {
   return (
     <View>
       <Svg width={width} height={height}>
-        <Text textAnchor="middle" fontSize={24}>
-          <TSpan x={width / 2} dy={128}>
-            <TSpan fill="red">Red</TSpan>
-            <TSpan fill="green">Green</TSpan>
-            <TSpan fill="blue">Blue</TSpan>
+        <Text textAnchor="middle" width={width} y={28} fontSize={16}>
+          <TSpan x={width / 2} dy={0}>
+            <TSpan fill="red">Nested</TSpan>
+            <TSpan fill="green">multiple</TSpan>
+            <TSpan fill="blue">TSpans</TSpan>
           </TSpan>
           <TSpan x={width / 2} dy={28}>
-            <TSpan fill="black">Second line</TSpan>
+            <TSpan fill="black">Nested single TSpan</TSpan>
           </TSpan>
+        </Text>
+        <Text textAnchor="middle" x={width / 2} y={128} fontSize={16}>
+          <TSpan fill="red">Top-level</TSpan>
+          <TSpan fill="green">multiple</TSpan>
+          <TSpan fill="blue">TSpans</TSpan>
+        </Text>
+        <Text textAnchor="middle" x={width / 2} y={156} fontSize={16}>
+          <TSpan fill="black">Top-level single TSpan</TSpan>
+        </Text>
+        <Text textAnchor="middle" x={width / 2} y={228} fontSize={16}>
+          <TSpan fill="red">Short</TSpan>
+          <TSpan fill="green">&amp; a bit longer</TSpan>
+          <TSpan fill="blue">overlap</TSpan>
         </Text>
       </Svg>
     </View>
