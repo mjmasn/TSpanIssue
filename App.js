4cbca50abd00b97b30a5a1443ea0b0fddfcccc01
Michael Mason <mjmasn@users.noreply.github.com>
2019-11-28 14:47:45 +0000

    Add some more cases

--- App.js
+++ App.js
@@ -17,27 +17,32 @@ const App: () => React$Node = () => {
       <Svg width={width} height={height}>
         <Text textAnchor="middle" width={width} y={28} fontSize={16}>
           <TSpan x={width / 2} dy={0}>
-            <TSpan fill="red">Nested</TSpan>
-            <TSpan fill="green">multiple</TSpan>
+            <TSpan fill="red">Text containing multiple</TSpan>
+            <TSpan fill="green">nested</TSpan>
             <TSpan fill="blue">TSpans</TSpan>
           </TSpan>
           <TSpan x={width / 2} dy={28}>
-            <TSpan fill="black">Nested single TSpan</TSpan>
+            <TSpan fill="black">Text containing single nested TSpan</TSpan>
           </TSpan>
         </Text>
         <Text textAnchor="middle" x={width / 2} y={128} fontSize={16}>
-          <TSpan fill="red">Top-level</TSpan>
+          <TSpan fill="red">Text containing</TSpan>
           <TSpan fill="green">multiple</TSpan>
           <TSpan fill="blue">TSpans</TSpan>
         </Text>
         <Text textAnchor="middle" x={width / 2} y={156} fontSize={16}>
-          <TSpan fill="black">Top-level single TSpan</TSpan>
+          <TSpan fill="black">Text containing single TSpan</TSpan>
         </Text>
         <Text textAnchor="middle" x={width / 2} y={228} fontSize={16}>
           <TSpan fill="red">Short</TSpan>
           <TSpan fill="green">&amp; a bit longer</TSpan>
           <TSpan fill="blue">overlap</TSpan>
         </Text>
+        <TSpan textAnchor="middle" x={width / 2} y={328} fontSize={16}>
+          <TSpan fill="red">Top-level</TSpan>
+          <TSpan fill="green">nested</TSpan>
+          <TSpan fill="blue">Tspans</TSpan>
+        </TSpan>
       </Svg>
     </View>
   );
